974cfc9da5ab5376daefc6f632fa216edc0e2626
midu16 <idumihai16@gmail.com>
2021-07-21 22:18:27 +0200

    remote socket communication and cli improvement

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import os
 import socket
 import psutil
 import time
+import socket
 import subprocess
 from subprocess import check_output
 
@@ -61,6 +62,61 @@ Checking the return code of the prometheus url
 """
 def CheckReturnCode():
     return 0
+
+"""
+making the remote communication socket for retrieving the status of the prometheus process
+"""
+def RemoteCheckProcess(second_host, remote_port):
+    slave = socket.socket()
+    slave.connect((second_host, remote_port))
+    # getoutput method executes the command and
+    # returns the output.
+    output += subprocess.getoutput(command)
+    # Encode and send the output of the command to
+    # the master machine.
+    slave.send(output.encode())
+    # close method closes the connection.
+    slave.close()
+
+def LocalCheckProcess(proc_name, remote_port):
+    # Create socket with socket class.
+    master = socket.socket()
+    # Host is the IP address of master
+    # machine.
+    host = "0.0.0.0"
+    # This will be the port that the
+    # socket is bind.
+    # binding the host and port to the
+    # socket we created.
+    master.bind((host, remote_port))
+    # listen method listens on the socket
+    # to accept socket connection.
+    master.listen(1)
+    # This method accept socket connection
+    # from the slave machine
+    slave, address = master.accept()
+    # When the slave is accepted, we can send
+    # and receive data in real time
+    # input the command from the user
+    command = CheckProcess(proc_name)
+    # encode the command and send it to the
+    # slave machine then slave machine can
+    # executes the command
+    slave.send(command.encode())
+    # If the command is exit, close the connection
+    #if command == "exit":
+    #    break
+    # Receive the output of command, sent by the
+    # slave machine.recv method accepts integer as
+    # argument and it denotes no.of bytes to be
+    # received from the sender.
+    output = slave.recv(5000)
+    print(output.decode())
+    # close method closes the socket connection between
+    # master and slave.
+    master.close()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # defining global variables
@@ -69,20 +125,23 @@ if __name__ == '__main__':
     global proc_name
     global main_host
     global second_host
+    global remote_port
     text = 'This is prometheus_healthcheck.\nThe following program is design to check the health of' \
            'another prometheus instance in a same environment.'
     parser = argparse.ArgumentParser(description=text)
-    parser.add_argument("-p", "--port", help="Add a new port of the Prometheus to check", default=9004)
+    parser.add_argument("-pp", "--prometheus-port", help="Add a new port of the Prometheus to check", default=9004)
     parser.add_argument("-pn", "--process-name", help="Mention the Prometheus string use to identify the process", default="prometheus")
     parser.add_argument("-v", "--version", action='version',version='%(prog)s ' + __version__)
     parser.add_argument("-mh", "--main-host", type=str, help="Prometheus main host")
     parser.add_argument("-sh", "--second-host", type=str, help="Prometheus second host")
+    parser.add_argument("-rp","--remote-port", help="Add a new port of the remote communication between main and second process", default=8989)
     args = parser.parse_args()
-    port = int(args.port)
+    port = int(args.prometheus_port)
     proc_name = args.process_name
     hostname = socket.gethostname()
     main_host = args.main_host
     second_host = args.second_host
+    remote_port = args.remote_port
     while True:
         if CheckProcess(proc_name) == False and CheckHost(hostname) == True:
             print(
