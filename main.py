543e57e1ca6217cb348fa660f32ce3284ed4b6b4
midu16 <idumihai16@gmail.com>
2021-07-23 13:43:38 +0200

    UDP socket communication validated

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ def CheckHost(hostname):
 """
 CheckPort is connecting to the Prometheus port and it validates that the remote and local prometheus instance is working.
 """
-def CheckPort(hostname, port):
+def CheckPort(remote_host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(2)  # 2 Second Timeout
-    result = sock.connect_ex((hostname, port))
+    result = sock.connect_ex((remote_host, port))
     if result == 0:
         return True
     else:
@@ -66,56 +66,51 @@ def CheckReturnCode():
 """
 making the remote communication socket for retrieving the status of the prometheus process
 """
-def RemoteCheckProcess(second_host, remote_port):
-    slave = socket.socket()
-    slave.connect((second_host, remote_port))
-    # getoutput method executes the command and
-    # returns the output.
-    output += subprocess.getoutput(command)
-    # Encode and send the output of the command to
-    # the master machine.
-    slave.send(output.encode())
-    # close method closes the connection.
-    slave.close()
+def RemoteSocket(proc_name, remote_port, remote_host, verbose_level):
+    localIP = remote_host
+    localPort = remote_port
+    bufferSize = 1024
+    msgFromServer = str(CheckProcess(proc_name))
+    bytesToSend = str.encode(msgFromServer)
 
-def LocalCheckProcess(proc_name, remote_port):
-    # Create socket with socket class.
-    master = socket.socket()
-    # Host is the IP address of master
-    # machine.
-    host = "0.0.0.0"
-    # This will be the port that the
-    # socket is bind.
-    # binding the host and port to the
-    # socket we created.
-    master.bind((host, remote_port))
-    # listen method listens on the socket
-    # to accept socket connection.
-    master.listen(1)
-    # This method accept socket connection
-    # from the slave machine
-    slave, address = master.accept()
-    # When the slave is accepted, we can send
-    # and receive data in real time
-    # input the command from the user
-    command = CheckProcess(proc_name)
-    # encode the command and send it to the
-    # slave machine then slave machine can
-    # executes the command
-    slave.send(command.encode())
-    # If the command is exit, close the connection
-    #if command == "exit":
-    #    break
-    # Receive the output of command, sent by the
-    # slave machine.recv method accepts integer as
-    # argument and it denotes no.of bytes to be
-    # received from the sender.
-    output = slave.recv(5000)
-    print(output.decode())
-    # close method closes the socket connection between
-    # master and slave.
-    master.close()
+    # Create a datagram socket
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+    # Bind to address and ip
+    UDPServerSocket.bind((localIP, localPort))
+    print("UDP server up and listening")
+
+    # Listen for incoming datagrams
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    message = bytesAddressPair[0]
+    address = bytesAddressPair[1]
+    while verbose_level == 2:
+        clientMsg = "Message from Client:{}".format(message)
+        clientIP = "Client IP Address:{}".format(address)
+        print(clientMsg)
+        print(clientIP)
+    # Sending a reply to client
+    UDPServerSocket.sendto(bytesToSend, address)
+
+
+def LocalSocket(proc_name, remote_port, remote_host, verbose_level):
+    msgFromClient = "Hello UDP Server"
+    bytesToSend = str.encode(msgFromClient)
+    serverAddressPort = (remote_host, remote_port)
+    bufferSize = 1024
+
+    # Create a UDP socket at client side
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    # Send to server using created UDP socket
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    while verbose_level == 2:
+        if str(msgFromServer[0].decode('ascii')) == "False":
+            print(
+                f'\n{time.strftime("%b %d %H:%M:%S")} Process [{proc_name}] is NOT running')
+        else:
+            print(
+                f'\n{time.strftime("%b %d %H:%M:%S")} Process [{proc_name}] is running')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -126,20 +121,21 @@ if __name__ == '__main__':
     global main_host
     global second_host
     global remote_port
+    global remote_host
     text = 'This is prometheus_healthcheck.\nThe following program is design to check the health of' \
            'another prometheus instance in a same environment.'
     parser = argparse.ArgumentParser(description=text)
     parser.add_argument("-pp", "--prometheus-port", help="Add a new port of the Prometheus to check", default=9004)
     parser.add_argument("-pn", "--process-name", help="Mention the Prometheus string use to identify the process", default="prometheus")
     parser.add_argument("-v", "--version", action='version',version='%(prog)s ' + __version__)
-    parser.add_argument("-mh", "--main-host", type=str, help="Prometheus main host")
+    parser.add_argument("-rh", "--remote-host", type=str, help="Prometheus main host")
     parser.add_argument("-sh", "--second-host", type=str, help="Prometheus second host")
     parser.add_argument("-rp","--remote-port", help="Add a new port of the remote communication between main and second process", default=8989)
     args = parser.parse_args()
     port = int(args.prometheus_port)
     proc_name = args.process_name
     hostname = socket.gethostname()
-    main_host = args.main_host
+    remote_host = args.remote_host
     second_host = args.second_host
     remote_port = args.remote_port
     while True:
@@ -148,7 +144,7 @@ if __name__ == '__main__':
                 f'\n{time.strftime("%b %d %H:%M:%S")} {hostname} Process [{proc_name}] is NOT running. The host {hostname} is up!')
         elif CheckProcess(proc_name) == True and CheckHost(hostname) == True:
             print(
-                f'\n{time.strftime("%b %d %H:%M:%S")} {hostname} Process [{proc_name}] with pid[{int(getpid("prometheus"))}] running')
+                f'\n{time.strftime("%b %d %H:%M:%S")} {hostname} Process [{proc_name}] with pid[{int(getpid(proc_name))}] running')
             print(CheckPort(hostname, port))
         else:
             print(f'\n{time.strftime("%b %d %H:%M:%S")} {hostname} Process is NOT running and host is down!\n'
